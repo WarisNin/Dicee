796a188826b3f397d18605f92cabf8871df86a0a
Waris Ninkaesorn <61102792+WarisNin@users.noreply.github.com>
2022-07-27 23:31:52 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random() * 6 + 1); //number 1-6
+var randomNumber2 = Math.floor(Math.random() * 6 + 1);
+document
+	.querySelector("img.img1")
+	.setAttribute("src", "images/dice" + randomNumber1 + ".png"); //dice1.png-dice6.png
+document
+	.querySelector("img.img2")
+	.setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+// If players wins
+if (randomNumber1 > randomNumber2) {
+	document.querySelector("h1").innerText = "🚩 Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+	document.querySelector("h1").innerText = "Player 2 Wins! 🚩";
+} else {
+	document.querySelector("h1").innerText = "Draw! 🤝";
+}
